fc27fd96e424de829ccf1eac4acc79537f0a1e6c
Mevlüt kaan karakoç <karakockaan326@gmail.com>
2025-01-05 21:44:32 +0300

    Player sınıfını ekledim

--- src/main.cpp
+++ src/main.cpp
@@ -223,3 +223,197 @@ class Decision{
       } 
  
 };
+
+class Player{
+      public:
+      int x;
+      int y;
+      int startx;
+      int starty;
+      int mapx;
+      int mapy;
+      bool running;
+      bool missionSuccess;
+      Directions current_direction;
+      Directions past_dir;
+      Directions crushed;
+
+      stack<Decision*> decisionpoints;
+      vector<Decision*> decisionpointsvec;
+      vector<Decision*> solutionpointsvec;
+      bool stopFlow;
+      Maze& maze;
+      Player(int x,int y,Maze& maze) : x(x),y(y),startx(x),starty(y),mapx(x),mapy(y),maze(maze){
+        running = true;
+        crushed = None;
+        missionSuccess = false;
+        stopFlow = false;
+        past_dir = None;
+      }
+
+      void move(Directions direction){
+            if(direction==Right)
+            {
+                if(maze.path.isInPath(x+1,y))
+                {
+                        x++;
+                        mapx+=2;
+                }
+                else{
+                    crushed = Right;
+                }
+            }
+            if(direction==Left)
+            {
+                if(maze.path.isInPath(x-1,y))
+                {
+                        x--;
+                        mapx-=2;
+                }
+                else{
+                    crushed = Left;
+                }
+            }
+            if(direction==Up)
+            {
+                if(maze.path.isInPath(x,y-1))
+                {
+                        y--;
+                        mapy-=1;
+                }
+                else{
+                    crushed = Up;
+                }
+            }
+            if(direction==Bottom)
+            {
+                if(maze.path.isInPath(x,y+1))
+                {
+                        mapy+=1;
+                        y++;
+                }
+                else{
+                    crushed = Bottom;
+                }
+            }
+            if(maze.detectEndPoint(x,y) && !(x==startx&&y==starty)){
+               missionSuccess = true;   
+            }
+      }
+
+      void setPosition(int x,int y){
+           this->x = x;
+           this->y = y;
+           this->mapx = 2*x;
+           this->mapy = y;
+      }
+
+
+      void handleKeyboard()
+      {
+            if (GetAsyncKeyState('W') & 0x8000) {  // Check if 'W' is pressed
+                this->move(Up);
+            }
+            else if (GetAsyncKeyState('A') & 0x8000) {  // Check if 'A' is pressed
+                this->move(Left);
+            }
+            else if (GetAsyncKeyState('S') & 0x8000) {  // Check if 'S' is pressed
+                this->move(Bottom);
+            }
+            else if (GetAsyncKeyState('D') & 0x8000) {  // Check if 'D' is pressed
+                this->move(Right);
+            }
+            else if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {  // Check if ESC is pressed
+                running = false;  // Exit the game loop
+            }
+      };   
+     
+      void handleKeyboardFlow()
+      {
+            if (GetAsyncKeyState('W') & 0x8000) {  // Check if 'W' is pressed
+                current_direction = Up;
+            }
+            else if (GetAsyncKeyState('A') & 0x8000) {  // Check if 'A' is pressed
+                current_direction = Left;
+            }
+            else if (GetAsyncKeyState('S') & 0x8000) {  // Check if 'S' is pressed
+                current_direction = Bottom;
+            }
+            else if (GetAsyncKeyState('D') & 0x8000) {  // Check if 'D' is pressed
+                current_direction = Right;
+            }
+            else if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {  // Check if ESC is pressed
+                running = false;  // Exit the game loop
+            }
+            
+            this->move(current_direction);
+      };
+       
+      bool isInDp()
+      {
+            for(Decision* p : decisionpointsvec)
+            {
+                  if(p->x == x && p->y ==y)
+                  {
+                    return true;
+                  }
+            }
+            return false;
+      }
+      void autoSolve()
+      {
+        if(!missionSuccess){
+                vector<Directions> dirs;
+                if(maze.path.isInPath(x+1,y) && past_dir!= Left ){dirs.push_back(Right);}
+                if(maze.path.isInPath(x-1,y) && past_dir!= Right){dirs.push_back(Left);}
+                if(maze.path.isInPath(x,y-1) && past_dir!= Bottom){dirs.push_back(Up);}
+                if(maze.path.isInPath(x,y+1) && past_dir!= Up){dirs.push_back(Bottom);}
+
+                if(dirs.size()>1 && !isInDp())
+                {                           
+
+                   Decision* p = new Decision(x,y,dirs);
+                   decisionpoints.push(p);
+                   decisionpointsvec.push_back(p);
+                   p->removeDir(dirs[0]);
+
+                }
+
+                if(dirs.size()>0){
+                    past_dir = dirs[0];
+                    this->move(dirs[0]);
+                }
+
+                else{
+                    Decision* p = decisionpoints.top();
+                    if(p->available.size()==0)
+                    {
+                        decisionpoints.pop();
+                        p = decisionpoints.top();
+                    }
+                    setPosition(p->x,p->y);
+                    past_dir = p->available[0];
+                    this->move(p->available[0]);
+                    p->removeDir(p->available[0]);
+                    p->usedCount ++;
+                    cout<<"["<<p->id<<"]"<<"teloporting to "<<p->x << " "<<p->y;
+                }
+        }
+        else{
+            while(!solutionpointsvec.empty())
+            {
+                solutionpointsvec.push_back(decisionpoints.top());
+                decisionpoints.pop();
+            }
+            
+        }
+
+      };
+
+      void renderPlayer(){
+           SetConsoleTextAttribute(hConsole,FOREGROUND_GREEN);
+           setCursorPosition(mapx,mapy);
+           cout<<"\033[42m  ";
+           SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+      }
+};
