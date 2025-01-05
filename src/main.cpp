fc8ce0681ac89620fbe244252766955c44446a53
Mevlüt kaan karakoç <karakockaan326@gmail.com>
2025-01-05 21:31:02 +0300

    Maze sınıfını ekledim

--- src/main.cpp
+++ src/main.cpp
@@ -71,3 +71,110 @@ class Path{
         this->points.push_back(new Point(x,y)); 
     } 
 };
+ 
+
+class Maze{
+      public:
+        vector<vector<char>> map;  // Vector to store the map (2D grid of chars)
+        Path path;    
+        int MazeHeight=-1;
+        int MazeWidth=-1;
+        vector<vector<Point>> detectedHorizontalLines;
+        vector<vector<Point>> detectedVerticalLines;
+        vector<Point*> InterceptionPoints; 
+        Maze()
+        {
+
+            ifstream inputFile("harita.txt");
+
+            if (!inputFile) {
+                cerr << "Error opening file!" << endl;
+            }
+
+            string line;  
+
+            while (getline(inputFile, line)) {
+                MazeHeight++;
+                MazeWidth = line.length();
+                vector<char> row;  
+                for (size_t i = 0; i < line.size(); i++) {
+                    row.push_back(line[i]);  
+                    if(line[i]==' '){
+                        path.addPoint(i,map.size());
+                    }
+                }
+                map.push_back(row); 
+            }
+
+
+            inputFile.close();
+            //detectLines();
+            /*
+            cout << "\nStored Horizontal Lines as List of Points:" << endl;
+            for (const auto& line : detectedHorizontalLines) {
+                for (const auto& pt : line) {
+                    cout << "(" << pt.x << ", " << pt.y << ") ";
+                }
+                cout << endl;
+            }
+
+            cout << "\nStored Vertical Lines as List of Points:" << endl;
+            for (const auto& line : detectedVerticalLines) {
+                for (const auto& pt : line) {
+                    cout << "(" << pt.x << ", " << pt.y << ") ";
+                }
+                cout << endl;
+            }*/
+        }   
+
+        ~Maze(){
+            for(auto&& point :  InterceptionPoints)
+            {
+                delete point;
+            }
+        }
+       
+
+        void renderMaze()
+        {
+
+    string mazeBuffer = "";
+
+    for (const auto& row : map) {
+        for (char c : row) {
+            if (c == '#') {
+                mazeBuffer += "\033[47m  \033[0m"; 
+                
+            } else {
+                mazeBuffer += "  ";
+            }
+        }
+        mazeBuffer += "\n"; 
+    }
+
+
+    setCursorPosition(0,0);
+    cout << mazeBuffer;
+
+        }
+
+        void showPoints(){
+            for(Point* p: path.points){
+                SetConsoleTextAttribute(hConsole,BACKGROUND_INTENSITY| FOREGROUND_GREEN);
+                setCursorPosition(p->x,p->y);
+                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY);
+                cout<<"*";
+            }            
+        }
+
+
+        bool detectEndPoint(int x,int y){
+              if(MazeHeight==y || y == 0 || MazeWidth == x || x==0)
+              {
+                 return true;
+              }
+              return false;
+        };
+
+
+};
